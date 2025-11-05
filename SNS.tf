d027936c0ed16039fa32ddc6137310e98a119c6c
DaanJanssens <555086@student.fontys.nl>
2025-11-05 14:30:09 +0100

    SNS sting problem solved?

--- SNS.tf
+++ SNS.tf
@@ -3,7 +3,7 @@ resource "aws_sns_topic" "ec2_down" {
 }
 
 resource "aws_sns_topic_subscription" "email_sub" {
-  topic_arn = aws_sns_topic.ec2_down
+  topic_arn = aws_sns_topic.ec2_down.arn
   protocol  = "email"
   endpoint  = "555086@student.fontys.nl"
 }
