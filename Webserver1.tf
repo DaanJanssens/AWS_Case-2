cb7f0bfcee0c2fcea6ddd3aaf3f901c5b33d4d8a
DaanJanssens <555086@student.fontys.nl>
2025-11-05 10:11:19 +0100

    backend for terraform state

--- Webserver1.tf
+++ Webserver1.tf
@@ -1,4 +1,12 @@
 terraform {
+backend "s3" {
+  bucket = "git-case2-terraform-bucket"
+  key = "global/s3/terrafrom.tfstate"
+  region = "eu-central-1"
+  encrypt = true
+  
+}
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
